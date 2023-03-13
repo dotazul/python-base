f155b02c1ba1dd7b4520b708f0e8f5cd234f6d71
dotazul <larissacamillybrisola@gmail.com>
2023-03-13 15:36:12 -0300

    + comentários

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""Aqui fica o nome do programa: Hello World Multii Linguas. 
 
+Aqui fica uma explicação do que o programa faz: Dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente. 
+
+As três aspas permite comentário de várias linhas como estou fazendo agora!
+
+"""
 # comentário de uma linha é só usar o "#"
 
 print("Hello, World!")
